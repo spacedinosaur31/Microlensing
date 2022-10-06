42487838b02f47b77b5904b67a046671036217b8
Lamiah Hammam <lamiah.hammam@kantiwattwil.ch>
2022-10-06 17:25:18 +0200

    new filter variations

---
+++ dotpyfiles/mergedfilter_jonas.py
@@ -0,0 +1,282 @@
+import pyarrow.parquet
+import pandas 
+import numpy 
+import os #functions for interacting with operating system
+from matplotlib import pyplot as plt
+from scipy.stats import skew
+import json
+
+# VALUES
+max_skew = -0.9  
+max_neumann = 1.5 # circa, in work
+
+# LISTS
+a_filtered_vorfilter = [] 
+a_filtered_grobfilter = []
+
+# dir = "C:\Kanti\Microlensing\Python\Parquet-Files" #path to root directory 
+dir = "./data" #path to root directory
+dir = "./Parquet-Files" #path to root directory
+
+class O_indices: #object for indices as "objectid", not as "lc[0]"
+    objectid = 0
+    filterid = 1
+    fieldid = 2
+    rcid = 3
+    objra = 4
+    objdec = 5
+    nepochs = 6
+    hmjd = 7
+    mag = 8
+    magerr = 9
+    clrcoeff = 10
+    catflags = 11
+
+o_indices = O_indices()
+
+# o_indices = {
+#     "objectid" : 0,
+#     "filterid" : 1,
+#     "fieldid" : 2,
+#     "rcid" : 3,
+#     "objra" : 4,
+#     "objdec" : 5,
+#     "nepochs" : 6,
+#     "hmjd" : 7,
+#     "mag" : 8,
+#     "magerr" : 9,
+#     "clrcoeff" : 10,
+#     "catflags" : 11,
+# }
+
+# a_s_column_name = [
+#      "objectid", # 
+#      "filterid", # 
+#      "fieldid",
+#      "rcid",
+#      "objra",
+#      "objdec",
+#      "nepochs",
+#      "hmjd",
+#      "mag",
+#      "magerr",
+#      "clrcoeff",
+#     "catflags",
+# ]
+
+# a_a_value__example = [
+#     #[...],
+#     [ 
+#        468316400000000, #=> objectid", # 
+#        3, #=> filterid", # 
+#        468, #=> fieldid",
+#        63, #=> rcid",
+#        148.4763641357422, #=> objra",
+#        7.4226508140563965, #=> objdec",
+#        1, #=> nepochs",
+#        array([58257.184], dtype=float32), #=> hmjd",
+#        array([17.160751], dtype=float32), #=> mag",
+#        array([0.02298518], dtype=float32), #=> magerr",
+#        array([0.18954925], dtype=float32), #=> clrcoeff",
+#        array([0, 1, 2, 4], dtype=uint16), #=> catflags",
+#     ], 
+#     #[...],
+# ]
+
+
+
+
+a_a_value__original_merged = None
+a_a_value__original_filtered = []
+
+for s_path_root, a_s_folder, a_s_file in os.walk(dir):
+    for s_file in a_s_file:
+        o_table = pyarrow.parquet.read_table(
+                os.path.join(s_path_root, s_file)
+            )
+        # print(o_table.schema.pandas_metadata)
+        o_parquet_json_decoded = o_table.schema.pandas_metadata
+        print(o_table.schema.pandas_metadata)
+        # o_parquet_file_json = json.loads(str(o_table.schema.pandas_metadata))
+        # print(o_parquet_file_json)
+        # print(o_table.schema.types)
+        o_pandas_data_frame = o_table.to_pandas()
+        # print(o_pandas_data_frame.values[0].dtype)
+        # print(type(o_pandas_data_frame.values[0][0]))
+        # a_a_type = [(o_pandas_data_frame.columns[n_index],numpy.dtype(o_parquet_json_decoded["columns"][n_index]["numpy_type"])) for (n_index, value) in enumerate(o_pandas_data_frame.values[0])]
+        a_a_type = [(o_pandas_data_frame.columns[n_index],o_parquet_json_decoded["columns"][n_index]["numpy_type"]) for (n_index, value) in enumerate(o_pandas_data_frame.values[0])]
+        # print(a_type)
+        # exit(1)
+
+        # exit(1)
+        
+        # exit(1)
+        # print(o_pandas_data_frame.columns)
+        # print(o_pandas_data_frame.__dict__)
+        a_a_value__original = o_pandas_data_frame.values #makes np.array()
+        
+        # if(a_a_value__original_merged == None):
+        #     a_a_value__original_merged = numpy.copy(a_a_value__original)
+        # else: 
+        #     a_a_value__original_merged = numpy.concatenate((a_a_value__original_merged, a_a_value__original))
+
+#make filtered list right away
+        a_a_value__filtered = [ 
+            # a_value # 'return value'
+            a_value# 'return value'
+            for
+            a_value # variable name in loop
+            in a_a_value__original  # array name of iterated array
+
+            if (
+                a_value[o_indices.catflags].sum() == 0 
+                and a_value[o_indices.nepochs] > 30
+                and a_value[o_indices.filterid] == 2 
+            )  # if this is true, 'return value' is returned
+        ]  
+
+        # o_pandas_data_frame_filtered = pandas.DataFrame(
+        #     a_a_value__filtered, 
+        #     columns = o_pandas_data_frame.columns
+        # )
+        # o_pandas_data_frame_filtered = pandas.DataFrame(
+        #     data = a_a_value__filtered,
+        #     index=o_pandas_data_frame.index,
+        #     columns=o_pandas_data_frame.columns
+        # )
+        # o_pandas_data_frame_filtered = pd.DataFrame().reindex_like(df_original)
+        # pd.DataFrame(
+        #     data=o_pandas_data_frame_filtered[1:,1:],    # values
+        #     index=data[1:,0],    # 1st column as index
+        #     columns=data[0,1:]
+        # )  # 1st row as the column names
+
+        # pyarrow.parquet.write_table(
+        #     "testparquestout", 
+        #     o_pandas_data_frame_filtered
+        # )
+
+        #     def filter_fromiter(arr, cond):
+        # return numpy.fromiter((x for x in arr if cond(x)), dtype=arr.dtype)
+        # a_a_value__filtered = numpy.array(a_a_value__filtered)
+
+        # o_dtype = numpy.dtype(o_pandas_data_frame.columns)
+        # print(a_type)
+        # print(a_type)
+        # print(a_type)
+        # exit(1)      
+        print(a_a_type)  
+        a_a_value__filtered = numpy.array(
+            a_a_value__filtered, 
+            # dtype=a_a_type
+            dtype=[('objectid', numpy.i64),
+            ('filterid', 'u8'),
+            ('fieldid', 'i16'),
+            ('rcid', 'u8'),
+            ('objra', 'f32'),
+            ('objdec', 'f32'),
+            ('nepochs', numpy.i64), 
+            ('hmjd', 'object'),
+            ('mag', 'object'),
+            ('magerr', 'object'),
+            ('clrcoeff', 'object'),
+            ('catflags', 'object')]
+            # [
+            #     (s,s)
+            #     for 
+            #     s 
+            #     in 
+            #     o_pandas_data_frame.columns
+            # ]
+            # o_pandas_data_frame.dtype
+        )    
+        # a_a_value__filtered.dtype = o_dtype
+        with open(f"./filtered/{s_file}_filtered.npy", 'wb') as o_file:
+            numpy.save(o_file, a_a_value__filtered)
+
+
+
+
+# 2 ^ 16 = 65000
+
+# 1111 1111 1111 1111 = 2 byes 
+# "65000"
+#  ^
+#   ^
+#    ^
+#     ^   5 bytes
+
+
+# print(a_a_value__original[0][o_indices.objectid])
+
+# print(a_a_value__original_merged)
+
+# a_a_value__filtered = [ 
+#     # a_value # 'return value'
+#     a_value# 'return value'
+#     for
+#     a_value # variable name in loop
+#     in a_a_value__original_merged  # array name of iterated array
+
+#     if (
+#         a_value[o_indices.catflags].sum() == 0 
+#         and a_value[o_indices.nepochs] > 3
+#     )  # if this is true, 'return value' is returned
+# ]
+
+# a_a_value__original_filtered = a_a_value__original_filtered + a_a_value__filtered
+
+# print("len(a_a_value__original)")
+# print(len(a_a_value__original))
+# print("len(a_a_value__filtered)")
+# print(len(a_a_value__filtered))
+
+# print(a_a_value__filtered_merged)
+
+# print(a_a_value__filtered)
+
+# 1.25 seconds per 2500 rows 
+
+        # a_a_value[]
+
+        # o_combinaion = o_house + o_dog 
+        # a_a_value[0] # 
+        # print(a_a_value)
+
+
+        # print(o_pandas_data_frame.values[0])
+
+        # allLC_list = df.values.tolist()  #convert pandas dataframe to list
+        # print(allLC_list[0][0])
+#         for lc in allLC_list: 
+#             #AB HIER FILTER EINFÜGEN
+#             print(lc)
+        
+
+#             catfl = lc[11] #type(catfl) = numpy.ndarray
+#             nepochs = lc[6] #Anzahl Messpunkte, type(nepochs) = int
+#             filterid = lc[1] #g (1), r (2) or i (3) -> only r, type(filter) = int
+#             if sum(catfl) == 0 and nepochs > 30 and filterid == 2: 
+#                 a_filtered_vorfilter.append(lc)
+
+# print(a_filtered_vorfilter)
+      
+# #Grobfilter: zu hohe Skewness & zu hoher Neumann-Statistik-Wert raus
+# for lc in a_filtered_vorfilter:  
+#     mag = lc[8]
+#     t = lc[7]
+#     neumann_lst = np.zeros(len(mag))
+#     std = np.std(mag)
+#     for i in range(1, len(mag)):
+#         neumann_lst[i] = ((mag[i] - mag[i-1])**2)/((len(mag)-1)*(std**2))
+#     n = np.sum(neumann_lst)
+#     if (skew(mag) < max_skew) and (n < max_neumann):
+#         a_filtered_grobfilter.append(lc)
+#         # plt.title(lc[0])
+#         # plt.plot(t, mag, ".", color = "red")
+        
+# print("LC-Menge nachher: ", len(a_filtered_grobfilter))
+
+# #save list in .txt for later use
+# with open('a_filtered_grobfilter.txt', 'w') as f:
+#     f.write(str(a_filtered_grobfilter))
---
+++ dotpyfiles/synthfilter6line.py
@@ -0,0 +1,143 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import norm, skew, kurtosis
+from scipy.optimize import curve_fit as fit
+import random
+
+"""
+Der Datensatz generiert Lichtkurvenliste der Anzahl LCamount. Jede Liste enthält 7 Einträge:
+Index 0: id
+1: Liste der Zeitpunkte
+2: Liste der Magnitudenwerte
+3: Falls ML-Event: umin-Wert
+   sonst: None
+4: Falls ML-Event: tE-Wert (Dauer des ML-Events)
+   sonst: None
+5: Skewness
+6: Neumann-Wert
+"""
+
+# VALUES
+probability = 1 # pro x (Probability) LC hat es ca. i ML-Event
+magpointamount_min = 30 # Mindestanzahl Messungen
+t0 = 0 # Zeitpunkt max. Helligkeit
+surveylength = 500 # wie lange gesamte Messung dauerte = maximale Eventdauer
+mean_skew = -0.22994225279524635  #Mittelwert von 2 fields 
+mean_std = 0.15856555  # Mittelwert von 2 fields 
+mean_mag = 20.421268  # Mittelwert von 2 fields
+mean_luminosity = 10**(mean_mag/(-2.5)) # convert magnitude-mean to luminosity-value for multiplication by amplification factor 
+mean_neumann = 1.6 # circa
+C = 0 # parameter for magnitude-calculation
+LCamount = 2000 # x LCs will be generated -> here: 2000 because 10h time to calculate overnight
+foundlostrelations_list = []
+skew_neumann_combinations = []
+
+# FUNCTIONS
+def theo(t, umin, tE): #theoretische ML-Funktion, returns mag
+    t_0 = 0
+    u = np.sqrt(umin**2 + ((t-t_0)/tE)**2)
+    A = mean_luminosity*((u**2 + 2) / (u*np.sqrt(u**2 + 4))) 
+    M = -2.5*np.log10(A) 
+    return M
+
+def neumann(array):
+    neumann_lst = np.zeros(len(array))
+    std = np.std(array)
+    for i in range(1, len(array)):
+        neumann_lst[i] = ((array[i] - array[i-1])**2)/((len(array)-1)*(std**2))
+    n = np.sum(neumann_lst)
+    return n
+
+for a in [x*0.1 for x in range(-20, -1)]: # range()-Funktion lässt keine float-steps zu
+    for b in [x*0.1 for x in range(10, 200)]:
+        #Lists 
+        lightc_lst = [0 for i in range(LCamount)] # stellt synthetischen Datensatz dar -> pro LC: Liste wie [Index (-> Object-ID), Magnitudenwerte, Zeitpunkte]
+        detect_lst_binary = np.zeros(LCamount) # Array gefilterter ML-Events, np.zeros(x) macht Liste mit x Nullen
+        truetruth_lst_binary = np.zeros(LCamount) # Array mit tatsächlichen ML-Events -> muss Liste sein, da np.array keine arrays speichern kann aber kein Problem, da bei ZTF kein lclist erzeugt werden muss
+        lightc_lst_filtered = [] # Index (wie object ID) wird in Liste eingefügt -> am Schluss gezählt, ob alle Kriterien erfüllt
+        umin_lost = []
+        tE_lost = []
+        umin_found = []
+        tE_found = []
+        lost = 0
+        trap = 0
+        found = 0
+
+        # SYNTHETIC LC GENERATOR + FILTERING -> Produktion künstlicher LC -> random ML-Events oder nicht 
+        for i in range(LCamount):   
+            ML_yesorno = random.randint(0, probability) #Wahrscheinlichkeit von 1/40
+            magpointamount = random.randint(magpointamount_min, 100)
+            if ML_yesorno == 1: #falls Microlensing-Event
+                t = np.zeros(magpointamount) # (start, end, Anz. Striche zwischen start und end) -> x-Achse
+                # freie Parameter hängen von t ab -> verändern sich mit der Zeit -> Körper bewegen sich
+                mag = np.zeros(magpointamount)
+                umin = random.random()
+                tE = random.randint(1, surveylength) # Zeitdauer, um Einstein-Radius zurückzulegen
+                for x in range(magpointamount):
+                    t[x] = random.randint(-(1/2)*surveylength, (1/2)*surveylength)
+                np.ndarray.sort(t)
+                for x in range(magpointamount):  # Helligkeit A abhängig von u, u abhängig t
+                    M = theo(t[x], umin, tE)  + random.gauss(0, mean_std) # bei beiden Gauss-Rauschen machen
+                    mag[x] = M
+                    lc_skew_value = skew(mag)
+                    lc_neumann_value = neumann(mag)
+
+                lightc_lst[i] = np.array([i, t, mag, umin, tE, lc_skew_value, lc_neumann_value], dtype = object)
+
+                # FILTER HERE:
+                if lc_neumann_value <= (a*lc_skew_value + b):
+                    found += 1
+                else:
+                    lost += 1
+
+                # plt.figure() #make coordinate system
+                # plt.plot(t, mag,".", color = "red")#t,mag = lists! -> A(t)+0.2*random -> adds random number to whole list -> for loop to handle each value separately!
+
+            else: 
+                
+                t = np.zeros(magpointamount) # (start, end, Anz. Striche zwischen start und end) -> x-Achse
+                # freie Parameter hängen von t ab -> verändern sich mit der Zeit -> Körper bewegen sich
+                mag = np.zeros(magpointamount)
+                for x in range(magpointamount):
+                    t[x] = random.randint(-(1/2)*surveylength, (1/2)*surveylength)
+                np.ndarray.sort(t)
+                for x in range(magpointamount):
+                    M = mean_mag + random.gauss(0, mean_std)
+                    mag[x] = M
+                    lc_skew_value = skew(mag)
+                    lc_neumann_value = neumann(mag)
+                
+                lightc_lst[i] = np.array([i, t, mag, None, None, lc_skew_value, lc_neumann_value], dtype = object)
+
+                # FILTER HERE:
+                if lc_neumann_value <= (a*lc_skew_value + b):
+                    trap += 1
+
+        foundlostrelations_list.append(found/((lost+1)*(trap+1))) # -> the bigger this value, the better the filter, lost/trap+1 because otherwise /zero
+        skew_neumann_combinations.append([a, b])
+                
+                # plt.figure() # make coordinate system
+                # plt.plot(t, mag,".", color = "red")#t,a = lists! -> A(t)+0.2*random -> adds random number to whole list -> for loop to handle each value separately!
+
+
+        # umin-tE-Diagramm:
+        # plt.figure()
+        # plt.plot(tE_lost, umin_lost, ".", color = "red")
+        # plt.plot(tE_found, umin_found, ".", color = "green")
+        # plt.xlabel("tE")
+        # plt.ylabel("umin")
+        # plt.title("max skew:" + str(max_skew) + "max neumann: " + str(max_neumann))
+        # plt.show()
+        #plt.savefig("umin_tE_plt.jpg")
+
+            # if np.count_nonzero(detect_lst_binary == 1) != 0: #wenn kein sog "Fehlversuch"
+            #     print("verlorene ML: ", lost)
+            #     print("scheinbare ML: ", trap)
+            #     print("gefundene ML: ", found)
+            #     print("skew ", max_skew, "neumann: ", max_neumann,"the bigger the better the filter: ", found/((lost + 1)*(trap+1))) # -> the bigger this value, the better the filter, trap+1 because otherwise /zero
+            #     # print(truetruth_lst_binary)
+            #     # print(detect_lst_binary)
+        # else:
+        #         print("Noch nicht vers8j8j888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888tandener scheinbarer Fehlversuch. Nochmal ausführen bis es klappt.")
+print("maximum relation: ", max(foundlostrelations_list))
+print(skew_neumann_combinations[foundlostrelations_list.index(max(foundlostrelations_list))])
